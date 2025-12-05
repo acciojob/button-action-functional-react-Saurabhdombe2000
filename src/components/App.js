aaa2481659fecabc27e38fab917ba919b1eeef66
Saurabhdombe2000 <saurabhdombe83@gmail.com>
2025-12-05 11:32:51 +0530

    button complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  let [showPara, setShowPara] = useState(false);
+
+  function handleClick() {
+    setShowPara(true);
+  }
   return (
     <div id="main">
-      // Do not alter the main div
+
+      <button id="click" onClick={handleClick}>Click me</button>
+
+      {showPara && (
+        <p id="para"> Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.</p>
+      )}
     </div>
   );
 }
